c9a5730be81253eb8e6ab4aa27f6ab288ae64643
djmailbox1829-arc <djmailbox1829@gmail.com>
2026-09-02 23:13:01 +0530

    Initial commit

---
+++ My_Langsmith_program.py
@@ -0,0 +1,10 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+prompt = ChatPromptTemplate.from_template("Explain {topic} in one simple sentence.")
+model = ChatOpenAI(model="gpt-4o-mini")
+chain = prompt | model | StrOutputParser()
+
+# This run is automatically recorded in LangSmith.
+print(chain.invoke({"topic": "LangSmith"}))
